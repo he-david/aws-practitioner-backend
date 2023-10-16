5c239978b08659afae410564d662028d077f9ecb
he-david <hegyidavid2001@gmail.com>
2023-10-16 21:42:51 +0200

    task-04: create dynamodb tables, use dynamodb to get and create products

--- product-service/src/libs/api-gateway.ts
+++ product-service/src/libs/api-gateway.ts
@@ -4,14 +4,14 @@ import type { FromSchema } from 'json-schema-to-ts';
 type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { body: FromSchema<S> };
 export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>;
 
-export const formatJSONResponse = (response: Record<string, unknown>) => {
+export const formatJSONResponse = (response: Record<string, unknown>, statusCode = 200) => {
   return {
-    statusCode: 200,
+    statusCode,
     body: JSON.stringify(response),
   };
 };
 
-export const formatErrorResponse = (error: Error, statusCode: number) => {
+export const formatErrorResponse = (error: Error, statusCode = 500) => {
   return {
     statusCode: statusCode,
     body: JSON.stringify({ message: error.message }),
